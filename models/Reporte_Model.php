20efd772acb7c30b75eec500f481e816b9bef2ef
German Guillen <goder@live.com>
2022-06-22 17:28:55 -0500

    update en base y en actividades

--- models/Reporte_Model.php
+++ models/Reporte_Model.php
@@ -3,13 +3,38 @@ require_once 'conection.php';
 
 function Actividades_DB($con, $id_area){
     $sql = "SELECT * FROM actividades a
-    INNER JOIN programaciones p ON p.id_atividad = a.id_actividad
+    LEFT JOIN programaciones p ON p.id_atividad = a.id_actividad
     WHERE a.id_area = $id_area";
     $stm = $con->query($sql);
     $actividades = $stm->fetchAll(PDO::FETCH_ASSOC);
     return $actividades;
 }
 
+function AvanceMes($con, $actividad, $mes){
+    $sqlav = "SELECT * FROM avances a
+    WHERE a.mes = $mes";
+    $stma = $con->query($sqlav);
+    $actividades = $stma->fetch(PDO::FETCH_ASSOC);
+    return $actividades;
+}
 
+
+if (isset($_POST['jfnkasjnkasdf34q345']) == "Enviar") {
+    $dependencia = $_POST;
+
+    foreach ($_FILES["imagenes"]["error"] as $clave => $error) {
+        if ($error == UPLOAD_ERR_OK) { 
+
+            //Falta validacion de tipo de documentos...
+
+            $nombre_tmp = $_FILES["imagenes"]["tmp_name"][$clave];
+            $nombre = basename("Hola".$_FILES["imagenes"]["name"][$clave]);
+            move_uploaded_file($nombre_tmp, "evidencias/$nombre"); // Comenzar por Anio, trimestre, y dependencia...
+        }
+    }
+    $sql = "INSERT INTO avances (mes, avance, justificacion, dir_evidenia, dir_evidenia_evidencia, id_actividad) VALUES (?,?,?,?,?,?)";
+    $sqlr = $con->prepare($sql);
+    $sqlr->execute(array());
+}
 
 ?>
